fd80b1c455002676edc183f9e9d9abc80ebc5992
Loris Aiulfi <loris.aiulfi@epfl.ch>
2016-10-30 20:19:51 +0100

    finished implementations_debug and debugged implementations.py

--- implementations.py
+++ implementations.py
@@ -75,6 +75,32 @@ def compute_loss(y, tx, w):
 
 def compute_rmse(y, tx, w):
     return np.sqrt(2 * compute_loss(y, tx, w))
+
+def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
+    """
+    Generate a minibatch iterator for a dataset.
+    Takes as input two iterables (here the output desired values 'y' and the input data 'tx')
+    Outputs an iterator which gives mini-batches of `batch_size` matching elements from `y` and `tx`.
+    Data can be randomly shuffled to avoid ordering in the original data messing with the randomness of the minibatches.
+    Example of use :
+    for minibatch_y, minibatch_tx in batch_iter(y, tx, 32):
+        <DO-SOMETHING>
+    """
+    data_size = len(y)
+
+    if shuffle:
+        shuffle_indices = np.random.permutation(np.arange(data_size))
+        shuffled_y = y[shuffle_indices]
+        shuffled_tx = tx[shuffle_indices]
+    else:
+        shuffled_y = y
+        shuffled_tx = tx
+    for batch_num in range(num_batches):
+        start_index = batch_num * batch_size
+        end_index = min((batch_num + 1) * batch_size, data_size)
+        if start_index != end_index:
+            yield shuffled_y[start_index:end_index], shuffled_tx[start_index:end_index]
+
     
 """ML algorithms"""
 
@@ -110,7 +136,7 @@ def least_squares_SGD(y, tx, initial_w, max_iters, gamma):
     
             n_iter+=1
     print("Gradient Descent({bi}/{ti}): loss={l}".format(
-              bi=max_epochs - 1, ti=max_epochs - 1, l=loss))        
+              bi=max_iters - 1, ti=max_iters - 1, l=loss))        
     return w, loss
 
 def least_squares(y, tx):
