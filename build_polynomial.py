cc8ef74b22ac588e206b68f3dd1b14575c694e1e
barry <martin.barry@epfl.ch>
2016-10-30 13:56:06 +0100

    formated code

--- build_polynomial.py
+++ build_polynomial.py
@@ -2,12 +2,26 @@
 """implement a polynomial basis function."""
 
 import numpy as np
+from math import *
 
 def build_poly(x, degree):
-    X=np.zeros((x.shape[0],degree*x.shape[1]+1))
+    n = x.shape[1]
+    dcross = np.int(factorial(n)/((factorial(n-2))*(factorial(2))))
+    X=np.zeros((x.shape[0],(degree+1)*x.shape[1]+1+2*dcross))
     for i in range(degree+1) :
         if i == 0 :
          X[:,i] = x[:,0]**i
         else  :
          X[:,(i-1)*x.shape[1]+1:i*x.shape[1]+1]=x**i
+    #print(degree*x.shape[1]+1)    
+    #print((degree+1)*x.shape[1]+1)
+    X[:,degree*x.shape[1]+1:(degree+1)*x.shape[1]+1]  = np.absolute(x)**0.5
+    m=0
+    for i in range (n) :
+        for j in range(i+1,n) :
+            m += 1
+            X[:,(degree+1)*x.shape[1]+m]  = x[:,i]*x[:,j]
+            m += 1
+            X[:,(degree+1)*x.shape[1]+m]  = np.absolute(x[:,i]*x[:,j])**0.5
+
     return X
--- gradient_descent.py
+++ gradient_descent.py
@@ -4,6 +4,16 @@ import numpy as np
 import costs as co
 
 
+def backtracing(y,tx,w,gradient,beta):
+    p =pow((np.linalg.norm(gradient)),2)
+    t = 1
+    loss = co.compute_loss(y,tx,w)
+    loss_mod = co.compute_loss(y,tx,w-t*gradient)
+    while(loss_mod > loss - t/2 *p):
+        loss_mod = co.compute_loss(y,tx,w-t*gradient)
+        t = beta*t
+    return t
+
 def compute_gradient(y, tx, w):
     """Compute the gradient."""
     e=(y-np.dot(tx,w))
@@ -21,13 +31,14 @@ def gradient_descent(y, tx, initial_w, max_iters, gamma):
     for n_iter in range(max_iters):
         grad = compute_gradient(y,tx,w)
         loss = co.compute_loss(y, tx, w)
-       
+        gamma = backtracing(y,tx,w,grad,0.3)
+        print(gamma)
         w=w-gamma*grad
         # store w and loss
         ws.append(np.copy(w))
         losses.append(loss)
-        if (n_iter+1)%max_iters==0 :
-         print("Gradient Descent({bi}/{ti}): loss={l}".format(
-              bi=n_iter , ti=max_iters - 1, l=loss))
+        #if (n_iter+1)%max_iters==0 :
+        print("Gradient Descent({bi}/{ti}): loss={l}".format(
+                bi=n_iter , ti=max_iters - 1, l=loss))
 
     return losses, ws
